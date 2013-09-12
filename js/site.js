b35fba8aa3588466c1ea671e45a7593f31a47c4c
Alexandra Hoefinger <alexandra.hoefinger@gmail.com>
2013-09-12 00:07:07 -0400

    first commit

--- js/site.js
+++ js/site.js
@@ -1,6 +1,56 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                totaldiv.text("too long!");
+            }
+        }
+    };
 
+  var number = "";
+  var newnumber = "";
+  var operator = "";
+  var totaldiv = $(".total");
+  totaldiv.text("0");
 
+  $(".numbers a").click(function(){
+    number += $(this).text();
+    totaldiv.text(number);
+    testNumLength(number);
+  });
 
+  $(".operators a").click(function(){
+    operator = $(this).text();
+    newnumber = number;
+    number = "";
+    totaldiv.text(newnumber);
+  });
 
+  $("#clear").click(function(){
+    number = "";
+    totaldiv.text("0");
+    if ($(this).attr("id") === "clear") {
+      newnumber = "";
+    }
+  });
+
+  $("#equals").click(function(){
+    if (operator === "+"){
+      number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
+    }
+    else if (operator === "-"){
+      number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(10);
+    }
+    else if (operator === "/"){
+      number = (parseInt(newnumber, 10) / parseInt(number,10)).toString(10);
+    }
+    else if (operator === "*"){
+      number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(10);
+    }
+    totaldiv.text(number);
+    testNumLength(number);
+    number = "";
+    newnumber = "";
+  });
 });
